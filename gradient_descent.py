037c134c1107cd273846ebf069d91ae15131518e
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2024-12-12 16:37:22 -0600

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -13,7 +13,7 @@ def gradient_descent(data, lr=0.0001, epochs=100000, tolerance=1e-6):
                 Y_pred_matrix = Matrix([Y_pred])
         
                 dm = 2 / n * X_matrix.multiply(Y_pred_matrix.resta(Y_matrix)).data
-                db = 2 / n * Y_pred_matrix.resta(Y_matrix).suma().data[0][0]
+                db = 2 / n * np.sum(Y_pred_matrix - Y_matrix)
                 
                 m_prev, b_prev = m, b
                 
