3e85d89c593354e649efe5862862d6e025f3aac0
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2024-12-12 16:35:59 -0600

    Update least_squares.py

--- least_squares.py
+++ least_squares.py
@@ -4,10 +4,10 @@ def least_squares(data):
         
         X_transpose = Matrix(X).transpose().data
         
-        X_transpose_X = Matrix(X_transpose).multiplicar(Matrix(X)).data
+        X_transpose_X = Matrix(X_transpose).multiply(Matrix(X)).data
         
-        X_transpose_Y = Matrix(X_transpose).multiplicar(Matrix([Y])).data
+        X_transpose_Y = Matrix(X_transpose).multiply(Matrix([Y])).data
         
-        theta = Matrix(X_transpose_X).eliminar_gaussiana([x[0] for x in X_transpose_Y])
+        theta = Matrix(X_transpose_X).gaussian_elimination([x[0] for x in X_transpose_Y])
         
         return theta[0], theta[1]
