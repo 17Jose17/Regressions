38ca511438af663ebee06b519befe8be66b439f6
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2024-12-12 16:36:26 -0600

    Update normal_equation.py

--- normal_equation.py
+++ normal_equation.py
@@ -3,9 +3,9 @@ def normal_equation(data):
         X, Y = data.to_matrix(degree=1)
         
         X_transpose = Matrix(X).transpose().data
-        X_transpose_X = Matrix(X_transpose).multiplicar(Matrix(X)).data
-        X_transpose_Y = Matrix(X_transpose).multiplicar(Matrix([Y])).data
+        X_transpose_X = Matrix(X_transpose).multiply(Matrix(X)).data
+        X_transpose_Y = Matrix(X_transpose).multiply(Matrix([Y])).data
         
-        theta = Matrix(X_transpose_X).eliminar_gaussiana([x[0] for x in X_transpose_Y])
+        theta = Matrix(X_transpose_X).gaussian_elimination([x[0] for x in X_transpose_Y])
         
         return theta[1], theta[0]
